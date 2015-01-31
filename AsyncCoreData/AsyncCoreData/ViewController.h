76f66eb6adeeaec45bf951de54748a1b617f8e27
kklovegui <xfhe19841104@gmail.com>
2015-01-31 21:02:51 +0800

    initial

---
+++ AsyncCoreData/AsyncCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AsyncCoreData
+//
+//  Created by Zhangweina on 31/1/15.
+//  Copyright (c) 2015 xiaofei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AsyncCoreData/AsyncCoreData/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AsyncCoreData
+//
+//  Created by Zhangweina on 31/1/15.
+//  Copyright (c) 2015 xiaofei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
